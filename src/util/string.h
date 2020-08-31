477faa46bdf8e24a7e2e66aaaaa4311f9874b9b3
Robert E Fry <robert.e.fry@protonmail.com>
2020-08-31 02:11:34 +0100

    Make utilities header-only

--- src/util/string.h
+++ src/util/string.h
@@ -6,10 +6,37 @@
 namespace util
 {
 
-    auto pad_string(const std::string&, size_t, char = ' ')
-        -> std::string;
-    auto pad_string_inplace(std::string&, size_t, char = ' ')
-        -> std::string&;
+    template <typename _Tp>
+    auto pad_string_inplace(std::basic_string<_Tp>& str, size_t size, _Tp pad = ' ')
+        -> std::basic_string<_Tp>&
+    {
+        if (str.size() > size) return str;
+
+        size_t pad_size = size - str.size();
+        return str.append(pad_size, pad);
+    }
+
+    template <typename _Tp>
+    auto pad_string(const std::basic_string<_Tp>& str, size_t size, _Tp pad = ' ')
+        -> std::basic_string<_Tp>
+    {
+        if (str.size() > size) return str;
+
+        size_t pad_size = size - str.size();
+        return str + std::basic_string<_Tp>(pad_size, pad);
+    }
+
+    template <typename _Tp>
+    auto repeat_string(const std::basic_string<_Tp>& str, size_t n)
+        -> std::basic_string<_Tp>
+    {
+        std::basic_string<_Tp> result;
+        result.reserve(str.size()*n);
+        for (size_t i = 0; i < n; ++i) {
+            result.append(str);
+        }
+        return result;
+    }
 
     auto repeat_string(const std::string&, size_t)
         -> std::string;
